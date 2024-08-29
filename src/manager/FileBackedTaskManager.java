6a85698239dd0c21e88c88b5c1374cd558cde23a
Oksana <volkova.oksana98@yandex.ru>
2024-08-29 08:59:08 +0300

    Исправить первый коммит 7-го спринта

--- src/manager/FileBackedTaskManager.java
+++ src/manager/FileBackedTaskManager.java
@@ -11,7 +11,6 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.List;
 
 public class FileBackedTaskManager extends InMemoryTaskManager {
 
